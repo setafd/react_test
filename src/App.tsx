c3a71bda56f73ef196aa06a0c74a554e7a0024af
Igor Gorin <45256548+setafd@users.noreply.github.com>
2022-06-05 13:21:42 +0300

    [Task-3] Configure react-redux && create simple example

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Example } from './exmpl/uselessExample';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,22 +8,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Example />
     </div>
   );
 }
